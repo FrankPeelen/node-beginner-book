13adeb14d3d979dda5d490e7bc93ad3482671227
Frank Christiaan Peelen <frank.peelen@yahoo.com>
2015-05-02 22:10:29 +0900

    Implement HTTP Response the right way

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,11 +1,20 @@
+var exec = require("child_process").exec;
+
 function start() {
 	console.log("Request handler 'start' was called.");
-	return "Hello Start";
+
+	exec("ls -lah", function (error, stdout, stderr) {
+		response.writeHead(200, {"Content-Type": "text/plain"});
+		response.write(stdout);
+		response.end();
+	});
 }
 
 function upload() {
 	console.log("Request handler 'upload' was called.");
-	return "Hello Upload";
+	response.writeHead(200, {"Content-Type": "text/plain"});
+	response.write("Hello Upload");
+	response.end();
 }
 
 exports.start = start;
