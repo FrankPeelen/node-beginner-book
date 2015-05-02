99e768b73bfb575d868521637e970eca0ce7acdc
Frank Christiaan Peelen <frank.peelen@yahoo.com>
2015-05-02 22:35:13 +0900

    Parse POST data with querystring

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,3 +1,5 @@
+var querystring = requite("querystring");
+
 function start(response, postData) {
 	console.log("Request handler 'start' was called.");
 
@@ -23,7 +25,7 @@ function start(response, postData) {
 function upload(response, postData) {
 	console.log("Request handler 'upload' was called.");
 	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write("You've sent: " + postData);
+	response.write("You've sent: " + querystring.parse(postData).text);
 	response.end();
 }
 
